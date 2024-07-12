6030158a2e6265d592c07c81e7a029a961dbd17f
TJ <thaddeusjiang@gmail.com>
2024-07-12 12:55:42 +0900

    feat: 🎸 render markdown
    TODO: code highlight

--- src/components/MessageBubble.tsx
+++ src/components/MessageBubble.tsx
@@ -1,8 +1,40 @@
-// FIXME: bug in extension: Uncaught Error: Cannot find module 'path'
-// import ReactMarkdown from "react-markdown"
-// import remarkGfm from "remark-gfm"
-
+import hljs from "highlight.js" // https://highlightjs.org/
+import { Remarkable } from "remarkable"
 import { twMerge } from "tailwind-merge"
+
+// TODO: code highlight still not working
+// copy from https://jonschlinkert.github.io/remarkable/demo/
+var md = new Remarkable({
+  html: false, // Enable HTML tags in source
+  xhtmlOut: false, // Use '/' to close single tags (<br />)
+  breaks: false, // Convert '\n' in paragraphs into <br>
+  langPrefix: "language-", // CSS language prefix for fenced blocks
+  linkify: true, // autoconvert URL-like texts to links
+  linkTarget: "", // set target to open link in
+
+  // Enable some language-neutral replacements + quotes beautification
+  typographer: false,
+
+  // Double + single quotes replacement pairs, when typographer enabled,
+  // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
+  quotes: "“”‘’",
+
+  // Highlighter function. Should return escaped HTML,
+  // or '' if input not changed
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value
+      } catch (__) {}
+    }
+
+    try {
+      return hljs.highlightAuto(str).value
+    } catch (__) {}
+
+    return "" // use external default escaping
+  }
+})
 
 export const MessageBubble = ({ message }: { message }) => {
   return (
@@ -12,7 +44,10 @@ export const MessageBubble = ({ message }: { message }) => {
         message.from_id === "me" ? " justify-end" : " justify-start"
       )}>
       <div className=" my-2  min-h-[2.5rem] max-w-[90%] overflow-hidden rounded-lg bg-blue-100 px-4 py-2">
-        <div className="prose break-words md:prose">{message.text ?? " "}</div>
+        <div
+          className="prose"
+          dangerouslySetInnerHTML={{ __html: md.render(message.text) }}
+        />
       </div>
     </div>
   )
